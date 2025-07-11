ca8b13ec8924687ab05c64d8b0ca89514875a268
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-07-11 14:38:47 +0200

    add 10-factorial.js file

---
+++ 10-factorial.js
@@ -0,0 +1,10 @@
+const firsArg = parseInt(process.argv[2]);
+
+function factorial(n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(firsArg));
