da6afe180bee56ee060a770f36dfc9c8ac0bbfe2
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-07-11 14:07:01 +0200

    fixing some errors

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ const arg = process.argv[2];
 const number = parseInt(arg);
 
 if (!isNaN(number)) {
-  console.log(` My number ${number}`);
+  console.log(`My number: ${number}`);
 } else {
   console.log("Not a number");
 }
